a38cb983a7bfc7aeb1df492bea4310ba0ba3fbe0
amine7777 <amine.tity@live.fr>
2018-11-05 03:21:26 +0100

    Change the model Dropout

--- cnn.py
+++ cnn.py
@@ -38,28 +38,17 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
+
 model = Sequential()
-
-
-model.add(Conv2D(64, kernel_size=(3, 3), activation = 'relu', input_shape = input_shape ))
-model.add(BatchNormalization(axis=1, epsilon=1e-05, momentum=0.9))
+model.add(Conv2D(32, kernel_size=(3, 3), activation = 'relu', input_shape = input_shape))
+model.add(Conv2D(64, (3, 3), activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.3))
-
-model.add(Conv2D(64, kernel_size=(3, 3), activation = 'relu', input_shape = input_shape ))
-model.add(BatchNormalization(axis=1, epsilon=1e-05, momentum=0.9))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.3))
-
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(BatchNormalization(axis=1, epsilon=1e-05, momentum=0.9))
-model.add(Dropout(0.3))
-model.add(Dense(64, activation='relu'))
-model.add(BatchNormalization(axis=1, epsilon=1e-05, momentum=0.9))
-model.add(Dropout(0.3))
-
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.25))
 model.add(Dense(num_classes, activation='softmax'))
+
 
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
 model.summary()
@@ -92,5 +81,4 @@ score = model.evaluate(x_test, y_test, verbose = 0)
 
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-# save model
-model.save("/home/amine/Desktop/SI/SystIntell/model_fashion-mnist_cnn_best_epochs" + str(epochs) + ".h5")
+
